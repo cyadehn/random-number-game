27307ecd6f5c41aea60c71b01a668262c63783cd
Chris Dehner <topherdehner@gmail.com>
2020-06-10 19:50:41 -0400

    removed unused code

--- updated-js-quiz/js/quiz.js
+++ updated-js-quiz/js/quiz.js
@@ -1,5 +1,4 @@
 //Provided functions
-
 //Prints message to document
 function print(message) {
   var outputDiv = document.getElementById('output');
@@ -8,13 +7,10 @@ function print(message) {
 
 
 //My Functions
-
 //Adds String with Correct vs. Incorrect Answer to incorrectString variable
 function addIncorrectAnswer(forLoop) {
   incorrectString += `<li>${quiz[forLoop][0]}<ul><li>Your answer: ${response}</li><li>Correct Answer: ${quiz[forLoop][1]}</li></ul></li>`
 }
-
-
 // Initial Quiz Array
 let quiz = [
   ['What number comes after 1?', '2'],
@@ -29,44 +25,25 @@ let incorrectAnswers = 0;
 let answer;
 let correctString = `<ol>`;
 let incorrectString = `<ol>`;
-
 //Added Variables
 let question;
 let response;
 
 
-//Original For Loop
-// for ( let i = 0; i < quiz.length; i += 1 ) {
-//   answer = prompt(`${quiz[i][0]}`);
-//   quiz[i][2] = `${answer}`;
-//   if ( answer.toLowerCase() === quiz[i][1] ) {
-//     correctAnswers += 1;
-//     correctString += `<li>${quiz[i][0]}<ul><li>Correct Answer: ${quiz[i][1]}</li></ul></li>`;
-//   } else {
-//     incorrectAnswers += 1;
-//     addIncorrectAnswer(i);
-//   }
-// }
-
 //Improved Function (Dave's)
 for ( let i = 0; i < quiz.length; i += 1 ) {
-  
   //Captures quiz data for readability
   question = quiz[i][0];
   answer = quiz[i][1];
-  
   //Capture prompt with question and initialize quiz array with response data
   response = prompt(question);
   quiz[i][2] = `${answer}`;
-  
   //Cond. statement checks if answer is correct or not
   if ( response.toLowerCase() === answer ) {
-    
     //Updates answer # and adds HTML to correctString
     correctAnswers += 1;
     correctString += `<li>${quiz[i][0]}<ul><li>Correct Answer: ${quiz[i][1]}</li></ul></li>`;
   } else {
-    
     //Updates answer # and calls addIncorrectAnswer function
     incorrectAnswers += 1;
     addIncorrectAnswer(i);
