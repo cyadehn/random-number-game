200672cbc39994c2b8748841e6ac9d0eb02d3468
Chris Dehner <topherdehner@gmail.com>
2020-06-20 14:29:07 -0400

    initial mini-grid display code

--- Tetris-Basic/app.js
+++ Tetris-Basic/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const ScoreDisplay = document.querySelector('#score');
     const StartBtn = document.querySelector('#start-button');
     const width = 10;
+    let nextRandom = 0;
 
     //Tetrominoes
     const lTetromino = [
@@ -68,11 +69,13 @@ document.addEventListener('DOMContentLoaded', () => {
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
             current.forEach(index => squares[currentPosition + index].classList.add('taken'));
             //start a new tetromino
-            random = Math.floor(Math.random() * theTetrominoes.length);
+            random = nextRandom;
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length);
             currentRotation = 0;
             current = theTetrominoes[random][currentRotation];
             currentPosition = 4;
             draw();
+            displayShape();
         }
     }
     
@@ -145,5 +148,25 @@ document.addEventListener('DOMContentLoaded', () => {
         if ( e.keyCode === 40 ) {
             moveDown();
         }
+    }
+
+    //show up-next tetromino in mini-grid
+    const displaySquares = document.querySelectorAll('.mini-grid div');
+    const displayWidth = 4;
+    let displayIndex = 0;
+
+    const upNextTetrominoes = [
+        [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+        [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino 
+        [displayWidth, 1, displayWidth+1, displayWidth+2], //tTetromino
+        [0, 1, displayWidth, displayWidth+1], //oTetromino
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+    ]
+
+    function displayShape() {
+        displaySquares.forEach( square => {square.classList.remove('tetromino')});
+        upNextTetrominoes[nextRandom].forEach( index => {
+            displaySquares[displayIndex + index].classList.add('tetromino');
+        })
     }
 })
