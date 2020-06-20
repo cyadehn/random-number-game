470d0b282b2db7017c0e344c1fab656d774a7ca4
Chris Dehner <topherdehner@gmail.com>
2020-06-20 11:44:07 -0400

    add tetromino arrays

--- Tetris-Basic/app.js
+++ Tetris-Basic/app.js
@@ -6,6 +6,37 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button');
     const width = 10;
 
-    
+    //Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [1, width+1, width+2, width+3]
+    ];
+    const zTetromino = [
+        [0, width, width+1, width*2+1],
+        [width*2, width+1, width*2+1, width+2],
+        [0, width, width+1, width*2+1],
+        [width*2, width+1, width*2+1, width+2]
+    ];
+    const tTetromino = [
+        [width, 1, width+1, width+2],
+        [1, width+1, width*2+1, width+2],
+        [width, width+1, width*2+1, width+2],
+        [width, 1, width+1, width*2+1]
+    ];
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ];
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ];
 
+    const tetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 })
