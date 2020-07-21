58627c0047e270d846dc26ebf10694d565ab09d7
Chris Dehner <topherdehner@gmail.com>
2020-07-21 00:37:26 -0400

    Feat: Detect mouse and touch events on grid display button

--- random/v3/js/5-event-handlers.js
+++ random/v3/js/5-event-handlers.js
@@ -1,19 +1,48 @@
+/* Event Handlers */
+const displayGuesses = (e) => {
+    const target = appWindow.displayGrid;
+    target.setAttribute("style", "display: none");
+    
+    if ( e.type == "mousedown" || e.type == "touchstart" ) {
+        target.setAttribute("style", "");
+        
+        //Mobile unfocuses keyboard to reveal grid
+        if (e.type == "touchstart") {
+            appWindow.commandLine.blur();
+        }
+    }
+
+    if ( e.type == "mouseup" || e.type == "touchend" ) {
+        target.setAttribute("style", "display: none")
+        
+        //Mobile refocuses on keyboard input
+        if (e.type == "touchend") {
+            appWindow.commandLine.focus();
+        }
+    }
+}
+
+/* Event Listeners */
 gridBtn.addEventListener(onclick/Touch, displayGuesses());
 gridBtn.addEventListener(clickoff/lift, displayGuesses());
 
-guessInput.addEventListener("input", () => {
+appWindow.commandLine.addEventListener("input", () => {
     guess = parseInt(guessInput.value);
   });
   
-  guessInput.addEventListener("keyup", (e) => {
+appWindow.commandLine.addEventListener("keyup", (e) => {
       e.preventDefault();
       if ( e.keyCode === 13 ) {
-          document.querySelector("#guess-submit").click();
+          appWindow.submit.click();
       }
   });
   
-  submitBtn.addEventListener("click", submit(e));
+  appWindow.submit.addEventListener("click", checkAnswer(e));
   
+  appWindow.displayGrid.addEventListener("mousedown", displayGuesses(e));
+  appWindow.displayGrid.addEventListener("mouseup", displayGuesses(e));
+  appWindow.displayGrid.addEventListener("touchstart", displayGuesses(e));
+  appWindow.displayGrid.addEventListener("touchend", displayGuesses(e));
   window.addEventListener("load", () => {
       prompt();
       guessInput.focus();
