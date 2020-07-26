7762f965c441d7dc00c76172549d5f20ae0ee258
Chris Dehner <topherdehner@gmail.com>
2020-07-26 01:58:31 -0400

    Feat: Scene data Constructor functions

---
+++ prev-guessing-games/TEST/cutscene-functions.js
@@ -0,0 +1,27 @@
+const flicker = () => {
+    console.info("Flickering...");
+}
+
+const scatter = () => {
+    console.info("Scattering...");
+}
+
+const reset = () => {
+    console.info("Resetting...");
+}
+
+const pause = () => {
+    console.info("Pausing...");
+}
+
+const burst = () => {
+    console.info("Bursting...");
+}
+
+const dialogue = () => {
+    console.info("Dialoguing...");
+}
+
+const endCutscene = () => {
+    console.info("Ending cutscene...");
+}
